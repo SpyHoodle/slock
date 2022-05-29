e71c4ac0106a438abb5acf4ae89b8518a9bf3682
Madeleine <32415621+SpyHoodle@users.noreply.github.com>
2022-05-29 20:00:03 +0100

    Update config.h

--- config.h
+++ config.h
@@ -1,6 +1,6 @@
 /* user and group to drop privileges to */
 static const char *user  = "spy";
-static const char *group = "spy";
+static const char *group = "users";
 
 /* colour definitions */
 static const char *colorname[NUMCOLS] = {
